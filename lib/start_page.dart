74be2f921dd087b2ddff27e4729411824af263e1
yukinkomob <yukinkopen@gmail.com>
2023-03-11 20:51:57 +0900

    Add signUp and signIn functions

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,13 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/diagnostics.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class StartPage extends StatelessWidget {
+class StartPage extends StatefulWidget {
   const StartPage({super.key});
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  bool _signInLoading = false;
+  bool _signUpLoading = false;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text("Start Page"),
-    ));
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    "https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png",
+                    height: 150.0,
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  // Email Field
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Field is required';
+                      }
+                      return null;
+                    },
+                    controller: _emailController,
+                    decoration: const InputDecoration(label: Text("Email")),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Field is required';
+                      }
+                      return null;
+                    },
+                    controller: _passwordController,
+                    decoration: const InputDecoration(label: Text("Password")),
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  // Signin Button
+                  _signInLoading
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : ElevatedButton(
+                          onPressed: () async {
+                            final isValid = _formKey.currentState?.validate();
+                            if (isValid != true) {
+                              return;
+                            }
+                            setState(() {
+                              _signInLoading = true;
+                            });
+                            try {
+                              await supabase.auth.signInWithPassword(
+                                  email: _emailController.text,
+                                  password: _passwordController.text);
+                            } catch (e) {
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(const SnackBar(
+                                content: Text("Sign in Failed"),
+                                backgroundColor: Colors.redAccent,
+                              ));
+                              setState(() {
+                                _signInLoading = false;
+                              });
+                            }
+                          },
+                          child: const Text("Sign In"),
+                        ),
+                  const Divider(),
+                  // Signup Button
+                  _signUpLoading
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : OutlinedButton(
+                          onPressed: () async {
+                            final isValid = _formKey.currentState?.validate();
+                            if (isValid != true) {
+                              return;
+                            }
+                            setState(() {
+                              _signUpLoading = true;
+                            });
+                            try {
+                              await supabase.auth.signUp(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                              );
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content:
+                                      Text("Success ! Confirmation email sent"),
+                                  backgroundColor: Colors.green,
+                                ),
+                              );
+                              setState(() {
+                                _signUpLoading = false;
+                              });
+                            } catch (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text("Sign up Failed"),
+                                  backgroundColor: Colors.redAccent,
+                                ),
+                              );
+                              setState(() {
+                                _signUpLoading = false;
+                              });
+                            }
+                          },
+                          child: const Text("Sign Up"),
+                        )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
